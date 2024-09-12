2a16f7b20bcff58fb76d407625a8be0342dfe29a
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-09-12 21:05:57 +0200

    Set up database connection settings and created Docker container configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
+import { PostsModule } from './posts/posts.module';
+import { KnexModule } from 'nestjs-knex';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    KnexModule.forRoot({
+      config: {
+        client: process.env.DB_CLIENT,
+        connection: {
+          database: process.env.DB_NAME,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+        },
+      },
+    }),
+    PostsModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('process.env.DB_CLIENT');
+    console.log(process.env.DB_NAME);
+  }
+}
